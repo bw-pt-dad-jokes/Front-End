be5bb7c2dbfc57590075e2e7a92ac8fbf51ed927
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-20 11:09:21 -0400

    Created basic login form with validation using yup

--- dadjokes/src/App.js
+++ dadjokes/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-
 import './App.scss';
 
 function App() {
   return (
     <div className='App'>
       <h1>App file</h1>
+     
     </div>
   );
 }
--- dadjokes/src/components/Login.js
+++ dadjokes/src/components/Login.js
@@ -1,21 +1,53 @@
 import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as yup from 'yup';
 
-
-export default function LoginPage ()  {
-
-    return (
-        <>
-        <div style={{
-        background: '#C4C4C4', 
-        height: '374px',
-        width: '533px',
-        margin: '100px auto',
-        borderRadius: '29px'
-        }}>
-            <div>
-
-            </div>
-        </div>
-        </>
-    )
+function LoginPage(props)
+ {
+	return (
+		<>
+			<div
+				style={{
+					background: '#C4C4C4',
+					height: '274px',
+					width: '333px',
+					margin: '100px auto',
+					borderRadius: '29px',
+				}}
+			>
+				<div style={{
+                    marginTop: '100px',
+                    padding: '70px'
+                }}>
+					<Form>
+                        {props.errors.username && props.touched.username && <p className="error">Username is a required field</p>}
+					  <Field name='username' placeholder='Username' style={{
+                            width: '200px'
+                        }}/> <br />
+                          {props.errors.password && props.touched.password && <p className="error">Password is a required field</p>}
+						<Field type='password' name='password' placeholder='Password' style={{
+                            width: '200px'
+                        }} /> <br />
+                        <button type="submit">Login Me In Scotty.....</button>
+					</Form>
+				</div>
+			</div>
+		</>
+	);
 }
+
+export default withFormik({
+	mapPropsToValues: (props) => {
+		return {
+            username: props.username || '',
+            password: props.password || '',
+		};
+    },
+validationSchema: yup.object().shape({
+    username: yup.string().required(),
+    password: yup.string().required().min(5)
+}),
+handleSubmit: (props) => {
+// axios request goes here
+}
+})(LoginPage);
