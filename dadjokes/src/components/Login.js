5bed27a615dbcb67050b9fb0d0003b75dc445a4c
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-20 12:28:26 -0400

    Added axios to package.json, working axios api call on submit button

--- dadjokes/src/components/Login.js
+++ dadjokes/src/components/Login.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
+import axios from 'axios';
 
-function LoginPage(props)
- {
+function LoginPage(props) {
 	return (
 		<>
 			<div
@@ -15,20 +15,33 @@ function LoginPage(props)
 					borderRadius: '29px',
 				}}
 			>
-				<div style={{
-                    marginTop: '100px',
-                    padding: '70px'
-                }}>
+				<div
+					style={{
+						marginTop: '100px',
+						padding: '70px',
+					}}
+				>
 					<Form>
-                        {props.errors.username && props.touched.username && <p className="error">Username is a required field</p>}
-					  <Field name='username' placeholder='Username' style={{
-                            width: '200px'
-                        }}/> <br />
-                          {props.errors.password && props.touched.password && <p className="error">Password is a required field</p>}
-						<Field type='password' name='password' placeholder='Password' style={{
-                            width: '200px'
-                        }} /> <br />
-                        <button type="submit">Login Me In Scotty.....</button>
+						{props.errors.username && props.touched.username && <p className='error'>{props.errors.username}</p>}
+						<Field
+							name='username'
+							placeholder='Username'
+							style={{
+								width: '200px',
+							}}
+						/>{' '}
+						<br />
+						{props.errors.password && props.touched.password && <p className='error'>{props.errors.password}</p>}
+						<Field
+							type='password'
+							name='password'
+							placeholder='Password'
+							style={{
+								width: '200px',
+							}}
+						/>{' '}
+						<br />
+						<button type='submit'>Login Me In Scotty.....</button>
 					</Form>
 				</div>
 			</div>
@@ -39,15 +52,21 @@ function LoginPage(props)
 export default withFormik({
 	mapPropsToValues: (props) => {
 		return {
-            username: props.username || '',
-            password: props.password || '',
+			username: props.username || '',
+			password: props.password || '',
 		};
-    },
-validationSchema: yup.object().shape({
-    username: yup.string().required(),
-    password: yup.string().required().min(5)
-}),
-handleSubmit: (props) => {
-// axios request goes here
-}
+	},
+	validationSchema: yup.object().shape({
+		username: yup.string().required('Username is required'),
+		password: yup
+			.string()
+			.required('Password is required')
+			.min(5, 'Password must be 5 char minimum'),
+	}),
+	handleSubmit: (props) => {
+        // axios request goes here
+        axios.post('https://reqres.in/api/users', props)
+        .then((res) => { console.log(res)})
+        .catch((err) => {console.log('Err', err)})
+	},
 })(LoginPage);
