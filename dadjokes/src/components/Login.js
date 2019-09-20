3583a4b4f22086874e35e757caea6b0bccc2a767
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-19 21:05:51 -0400

    created Login.js and added div container for form, added node-sass, formik, and yup packages

---
+++ dadjokes/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default function LoginPage ()  {
+
+    return (
+        <>
+        <div style={{
+        background: '#C4C4C4', 
+        height: '374px',
+        width: '533px',
+        margin: '100px auto',
+        borderRadius: '29px'
+        }}>
+            <div>
+
+            </div>
+        </div>
+        </>
+    )
+}
