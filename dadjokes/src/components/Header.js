30a77355952ce4ba9f204a4f1c492b74840b90ac
Mikey Williams <manicdesigner@gmail.com>
2019-09-21 17:59:57 -0400

    add link text

--- dadjokes/src/components/Header.js
+++ dadjokes/src/components/Header.js
@@ -9,8 +9,8 @@ function Header() {
 		<div className='headerWrapper'>
 			<h1 className='headline'>DAD JOKES</h1>
 			<nav className='navigation'>
-				<NavButton buttonText='Sign Up' />
-				<NavButton buttonText='Log In' />
+				<NavButton linkText='/signup' buttonText='Sign Up' />
+				<NavButton linkText='/login' buttonText='Log In' />
 			</nav>
 		</div>
 	);
