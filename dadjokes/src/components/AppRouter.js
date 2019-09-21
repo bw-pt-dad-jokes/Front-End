010842b6bca988dacd89571eadb79c7d37f74d01
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2019-09-21 19:32:32 -0400

    added router to index.js, created new Home component, updated AppRouter with route to Home component, removed unecessary imports from App.js

--- dadjokes/src/components/AppRouter.js
+++ dadjokes/src/components/AppRouter.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp.js';
+import Home from './Home.js';
 
 export default function AppRouter() {
     return (
-        <>
+        <>  
+            <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
         </>
