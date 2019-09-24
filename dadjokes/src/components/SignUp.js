4b6ad8a87fe7c6a58333ba70544ada2b04dc7eb3
Michael Williams <manicdesigner@gmail.com>
2019-09-23 22:20:09 -0400

    ron helped with error message from semantic-ui instead of scss style error message

--- dadjokes/src/components/SignUp.js
+++ dadjokes/src/components/SignUp.js
@@ -2,32 +2,30 @@ import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
-import { Button, Form as Forms, Label, Container, Header } from 'semantic-ui-react';
+import { Button, Form as Forms, Label, Container, Header, Message } from 'semantic-ui-react';
 import './SignupLogin.scss';
 
 function SignUp({ errors, touched, status }) {
 	return (
 		<Container>
-			<div>
-				<Header as='h3' className='sign-up'>
-					To see all Dad Jokes Please sign up for an an account:
-				</Header>
-				<Form>
-					{touched.email && errors.email && <p className='error-message'>{errors.email}</p>}
+			<Header as='h3' className='sign-up'>
+				To see all Dad Jokes Please sign up for an an account:
+			</Header>
+			<Form className='form-wrapper'>
+				{touched.email && errors.email && <Message color='red'>{errors.email}</Message>}
 
-					<Forms.Field>
-						<Label htmlFor='user-email'>User Email</Label>
-						<Field input='email' id='user-email' name='email' placeholder='Email@address.com' />
-					</Forms.Field>
-					{touched.password && errors.password && <p className='error-message'>{errors.password}</p>}
-					<Forms.Field>
-						<Label htmlFor='user-password'>Password</Label>
-						<Field input='password' id='user-password' name='password' placeholder='123abc@' />
-					</Forms.Field>
+				<Forms.Field>
+					<Label htmlFor='user-email'>User Email</Label>
+					<Field input='email' id='user-email' name='email' placeholder='Email@address.com' />
+				</Forms.Field>
+				{touched.password && errors.password && <Message color='red'>{errors.password}</Message>}
+				<Forms.Field>
+					<Label htmlFor='user-password'>Password</Label>
+					<Field input='password' id='user-password' name='password' placeholder='123abc@' />
+				</Forms.Field>
 
-					<Button type='submit'>Sign Me up Scotty....</Button>
-				</Form>
-			</div>
+				<Button type='submit'>Sign Me up Scotty....</Button>
+			</Form>
 		</Container>
 	);
 }
