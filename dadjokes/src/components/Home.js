f341948195febc485fba852e4596aa1fb28f6cac
Michael Williams <manicdesigner@gmail.com>
2019-09-25 13:07:46 -0400

    Button to display jokes after click buttons and puns disappears & jokes display

--- dadjokes/src/components/Home.js
+++ dadjokes/src/components/Home.js
@@ -1,14 +1,16 @@
-import React from 'react'
-
-
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-
-    return (
-        <>
-        <div>
-            <h1>"just the puns...."</h1>
-        </div>
-        </>
-    )
+	return (
+		<>
+			<div>
+				<h1>"just the puns...."</h1>
+				<Button as={Link} to='/jokes' size='huge'>
+					Show the Yucks...
+				</Button>
+			</div>
+		</>
+	);
 }
