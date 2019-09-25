7e2eee3825c7307240e63a3a6bb99c4a6877c3af
Michael Williams <manicdesigner@gmail.com>
2019-09-25 13:05:29 -0400

    add JokesDisplay route into AppRouter

--- dadjokes/src/components/AppRouter.js
+++ dadjokes/src/components/AppRouter.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp.js';
 import Home from './Home.js';
+import JokesDisplay from './JokesDisplay';
 
 export default function AppRouter() {
-    return (
-        <>  
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={SignUp} />
-        </>
-    )
+	return (
+		<>
+			<Route exact path='/' component={Home} />
+			<Route path='/jokes' component={JokesDisplay} />
+			<Route path='/login' component={Login} />
+			<Route path='/signup' component={SignUp} />
+		</>
+	);
 }
