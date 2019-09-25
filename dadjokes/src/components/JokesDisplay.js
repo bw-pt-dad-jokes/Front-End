53d96a01c8e04013ebffcc02d2f1e937dfe6a7dd
Michael Williams <manicdesigner@gmail.com>
2019-09-25 13:06:05 -0400

    styled up JokesDisplay some w/ styled-components

--- dadjokes/src/components/JokesDisplay.js
+++ dadjokes/src/components/JokesDisplay.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container, Grid, Card } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 function JokesDisplay() {
 	const [jokes, setJokes] = useState([]);
@@ -14,22 +15,40 @@ function JokesDisplay() {
 	console.log(jokes);
 
 	return (
-		<>
+		<JokeWrapper>
 			{jokes.map((joke) => {
 				return (
-					<Grid key={joke.id}>
-						<Grid.Column mobile={12} tablet={6}>
-							<Card>
-								<Card.Content>
-									<Card.Description>{joke.joke}</Card.Description>
-								</Card.Content>
-							</Card>
-						</Grid.Column>
+					<Grid key={joke.id} centered columns={4}>
+						<Grid.Row>
+							<JokerGrid>
+								<JokeCard>
+									<Card.Content>
+										<Card.Description>{joke.joke}</Card.Description>
+									</Card.Content>
+								</JokeCard>
+							</JokerGrid>
+						</Grid.Row>
 					</Grid>
 				);
 			})}
-		</>
+		</JokeWrapper>
 	);
 }
 
 export default JokesDisplay;
+
+const JokeWrapper = styled.div`
+	margin-top: 45px;
+`;
+
+const JokerGrid = styled(Grid.Column)`
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+const JokeCard = styled(Card)`
+	margin-right: auto;
+	margin-left: auto;
+	max-width: 675px !important;
+	min-width: 300px;
+`;
